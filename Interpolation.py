fb7e3ee056fcbcdb0f301fa7f65187dd89da1c57
Lorenzo <lorenzoterenzi96@gmail.com>
2017-02-25 22:13:48 +0100

    Added Gauss lobatto nodal pts

--- Interpolation.py
+++ Interpolation.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.special import legendre
+from Root_finding import newton_method
+from functools import partial
+import scipy as sc
+from Root_finding import recursive_bisection
 
 def f(x):return np.sin(np.pi*x/2.)
 def f_poly5(x): return 0.001*x**5 + 0.02*x**3 - x  # Polynomial
@@ -15,7 +19,7 @@ def f_gauss(x): return np.exp(-(x-2)**2/2.)        # Very similar curve to above
 
 def grid_uniform(a,b,n):
     # n is the number of elements in the grid
-    return np.linspace(a,b,n+1)
+    return np.linspace(a,b,n)
 
 
 def grid_chebychev(a,b,n):
@@ -23,7 +27,60 @@ def grid_chebychev(a,b,n):
     # there exist a convient formula to find the root of the Ch. polynomial
     i = np.arange(0,n+1)
     # nodal point in [-1,1]
-    nodal_pts = np.cos((2*i + 1)/(2*(n+1))*np.pi)
+    nodal_pts = np.cos((2*i+1)/(2*(n+1))*np.pi)
+    return (-nodal_pts+1)*(b-a)/2 + a
+
+
+def legendre_prime(x,n):
+    # P'_n+1 = (2n+1) P_n + P'_n-1
+    # where P'_0 = 0 and P'_1 = 1
+    # source: http://www.physicspages.com/2011/03/12/legendre-polynomials-recurrence-relations-ode/
+    if n == 0:
+        if isinstance(x, np.ndarray):
+            return np.zeros(len(x))
+        elif isinstance(x,(int, float)):
+            return 0
+    if n == 1:
+        if isinstance(x, np.ndarray):
+            return np.ones(len(x))
+        elif isinstance(x, (int, float)):
+            return 1
+    # legendre_p = (2*(n-1)+1)*legendre(n-1)(x) + legendre_prime(x, n-2)
+    legendre_p = n*legendre(n-1)(x)-n*x*legendre(n)(x)
+    return legendre_p*(1-x**2)
+
+# def legendre_prime_adjusted(x,n):
+#     return legendre_prime(x,n)*(1-x**2)
+
+
+def legendre_double_prime_num(x,n):
+    # The result is highly inaccurate and causes newton rapson to diverge
+    legendre_p = partial(legendre_prime, n=n)
+    legendre_pp = np.zeros(np.size(x))
+    if isinstance(x, np.ndarray):
+        for i, x_i in enumerate(x):
+            legendre_pp[i] = sc.misc.derivative(legendre_p,x_i)
+    else:
+        legendre_pp = sc.misc.derivative(legendre_p,x)
+    return legendre_pp
+
+def legendre_double_prime_recursive(x,n):
+    legendre_pp = 2 * x * legendre_prime(x,n) - n*(n+1)*legendre(n)(x)
+    return legendre_pp * (1-x**2)
+
+
+def grid_gauss_lobatto(a, b, n):
+    # roots of chebichev polynomials
+    x_0 = np.cos(np.arange(1,n)/n*np.pi)
+    # print('second derivatives recursive : ', legendre_double_prime_recursive(x_0,n))
+    # print('second derivatives numerical : ', legendre_double_prime_num(x_0,n))
+    nodal_pts = np.zeros(len(x_0)+2)
+    for i, ch_pt in enumerate(x_0):
+        leg_p = partial(legendre_prime, n=n)
+        leg_pp = partial(legendre_double_prime_recursive, n=n)
+        nodal_pts[i+1] = newton_method(leg_p, leg_pp, ch_pt, 40)[0]
+    nodal_pts[0] = -1
+    nodal_pts[-1] = 1
     return (-nodal_pts+1)*(b-a)/2 + a
 
 
@@ -54,6 +111,7 @@ def basis_newton(x, grid):
         for j in range(i):
             phi[i,:] *= (x-grid[j])
     return phi
+
 
 def basis_lagrange(x,grid):
     phi = np.ones((len(grid), len(x)))
@@ -89,7 +147,6 @@ def find_interpolation_coeff(type, grid, f):
 
 # reconstruction
 
-
 def reconstruct(coeff, phi):
     return coeff.dot(phi)
 
@@ -117,9 +174,27 @@ def plot_funcs(funcs):
         print('Error max: ', error)
 
 if __name__ == '__main__':
-    grid = grid_chebychev(-2,2,2)
-    grid_2 = grid_uniform(-2,2,3)
-    x = np.linspace(-2,2,101)
+    pass
+    # grid = grid_chebychev(-1,1,3)
+    # grid_2 = grid_uniform(-2,2,3)
+    # print('Chebishev grid ', grid)
+    # x = np.linspace(-1,1,1001)
+    # plot_grid(grid,show=False)
+
+    # testing legendre derivative and closeness of chebichev
+    # legendre_p = legendre_prime(x,2)
+    # legendre_pp = legendre_double_prime(x,5)
+    # legendre_pp2 = legendre_double_prime_num(x,3)
+    # plt.plot(x,legendre_p),\
+    # plt.plot(x,legendre_pp),\
+    # plt.plot(x, legendre_pp2),
+
+    # # testing gauss lobatta
+    # grid_lobatto = grid_gauss_lobatto(-1,1,2)
+    # #
+    # print(grid_lobatto)
+    # plot_grid(grid_lobatto)
+
     # phi_mon = basis_monomial(x,grid_2)
     # phi_new = basis_newton(x,grid_2)
     # phi_lag = basis_lagrange(x,grid_2)
@@ -128,11 +203,13 @@ if __name__ == '__main__':
     # plot_basis(x,phi_lag, grid_2)
     # print(basis_lagrange([grid_2[1]],grid_2))
     # plot_grid([grid, grid_2])
+
     # coeff, A = find_interpolation_coeff('lagrange',grid_2,f)
     # phi = basis_lagrange(x,grid_2)
     # interpolated_f = reconstruct(coeff,phi)
     # error = plot_interpolation(f,interpolated_f,x)
+
     # # error tends to decrease with the Chebischev nodes
     # print('Error max: ', error)
-    funcs = [f,f_poly5,f_abs,f_abs3,f_gauss,f_runge,f_step]
-    plot_funcs(funcs)
+    # funcs = [f,f_poly5,f_abs,f_abs3,f_gauss,f_runge,f_step]
+    # plot_funcs(funcs)
--- test_interpolation.py
+++ test_interpolation.py
@@ -11,16 +11,25 @@ class Test_lagrange_orthogonality(unittest.TestCase):
                 if i != j:
                     self.assertAlmostEquals(phi[j],0)
 
+
 class Test_basis_matrix_lagrange(unittest.TestCase):
     def test(self):
         grid = grid_chebychev(-2,2,5)
-        coeff, A = find_intepolation_coeff('lagrange', grid,f)
+        coeff, A = find_interpolation_coeff('lagrange', grid,f)
         for i in range(len(grid)):
             for j in range(len(grid)):
                 if i == j:
                     self.assertAlmostEqual(A[i,j],1)
                 else:
                     self.assertAlmostEqual(A[i,j],0)
+
+
+class Test_gauss_lobatto(unittest.TestCase):
+    def test(self):
+        for i in range(2,20):
+            gauss_lobatto_pts = grid_gauss_lobatto(-1,1,i)
+            for pt in gauss_lobatto_pts:
+                self.assertAlmostEqual(legendre_prime(pt,i),0,places=13)
 
 
 
