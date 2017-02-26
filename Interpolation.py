50d58b2fb1e442b25bac3f895d621b3bd8e50775
Lorenzo <lorenzoterenzi96@gmail.com>
2017-02-26 13:20:51 +0100

    Class sturcture implemented

--- Interpolation.py
+++ Interpolation.py
@@ -1,12 +1,8 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import legendre
-from Root_finding import newton_method
-from functools import partial
-import scipy as sc
-from Root_finding import recursive_bisection
+from legendre_functions import *
+from grid import Grid
 
-def f(x):return np.sin(np.pi*x/2.)
+def f(x): return np.sin(np.pi*x/2.)
 def f_poly5(x): return 0.001*x**5 + 0.02*x**3 - x  # Polynomial
 def f_step(x): return np.sign(x-1)                 # Discontinuous at x=2
 def f_abs(x): return np.abs(x-2)                   # Discontinuous derivative at x=2
@@ -14,163 +10,63 @@ def f_abs3(x): return np.abs((x-2)**3)             # Discontinuous 3rd derivativ
 def f_runge(x): return 1./(1+(x-2)**2)             # Infinitely differentiable (everywhere)
 def f_gauss(x): return np.exp(-(x-2)**2/2.)        # Very similar curve to above
 
-# Grid
 
+class PolyBasis:
 
-class Grid:
+    def __init__(self, x, grid):
+        self.x = x
+        self.grid = grid
+        self.nodal_pts = grid.nodal_pts
+        # degree of the basis
+        self.n = len(self.nodal_pts)
+        self.basis = np.ones((len(self.nodal_pts), len(x)))
 
-    def __init__(self,start,end,num):
-        self.a = start
-        self.b = end
-        self.n = num
-        self.nodal_pts = np.empty(num+1)
+    def monomial(self):
+        self.basis = np.zeros((self.n, len(self.x)))
+        for i in range(self.n):
+            self.basis[i,:] = self.x**i
 
-    def uniform(self):
-        return np.linspace(self.a, self.b, self.n)
+    def newton(self):
+        self.basis = np.ones((self.n, len(self.x)))
+        for i in range(self.n):
+            for j in range(i):
+                self.basis[i,:] *= (self.x-self.nodal_pts[j])
 
-    def chebychev(self):
-        # chebychev interpolation nodes impose the minimal upper bound on the monic polinomial
-        # there exist a convient formula to find the root of the Ch. polynomial
-        i = np.arange(0, n + 1)
-        # nodal point in [-1,1]
-        self.nodal_pts = np.cos((2 * i + 1) / (2 * (n + 1)) * np.pi)
-        return (-self.nodal_pts + 1) * (b - a) / 2 + a
+    def lagrange(self):
+        self.basis = np.ones((self.n, len(self.x)))
+        for j in range(self.n):
+            for i in range(self.n):
+                if i != j:
+                    self.basis[j] *= (self.x-self.nodal_pts[i])/(self.nodal_pts[j]-self.nodal_pts[i])
 
-
-
-
-def grid_uniform(a,b,n):
-    # n is the number of elements in the grid
-    return np.linspace(a,b,n)
-
-
-def grid_chebychev(a,b,n):
-    # chebychev interpolation nodes impose the minimal upper bound on the monic polinomial
-    # there exist a convient formula to find the root of the Ch. polynomial
-    i = np.arange(0,n+1)
-    # nodal point in [-1,1]
-    nodal_pts = np.cos((2*i+1)/(2*(n+1))*np.pi)
-    return (-nodal_pts+1)*(b-a)/2 + a
-
-
-def legendre_prime(x,n):
-    # P'_n+1 = (2n+1) P_n + P'_n-1
-    # where P'_0 = 0 and P'_1 = 1
-    # source: http://www.physicspages.com/2011/03/12/legendre-polynomials-recurrence-relations-ode/
-    if n == 0:
-        if isinstance(x, np.ndarray):
-            return np.zeros(len(x))
-        elif isinstance(x,(int, float)):
-            return 0
-    if n == 1:
-        if isinstance(x, np.ndarray):
-            return np.ones(len(x))
-        elif isinstance(x, (int, float)):
-            return 1
-    # legendre_p = (2*(n-1)+1)*legendre(n-1)(x) + legendre_prime(x, n-2)
-    legendre_p = n*legendre(n-1)(x)-n*x*legendre(n)(x)
-    return legendre_p*(1-x**2)
-
-# def legendre_prime_adjusted(x,n):
-#     return legendre_prime(x,n)*(1-x**2)
-
-
-def legendre_double_prime_num(x,n):
-    # The result is highly inaccurate and causes newton rapson to diverge
-    legendre_p = partial(legendre_prime, n=n)
-    legendre_pp = np.zeros(np.size(x))
-    if isinstance(x, np.ndarray):
-        for i, x_i in enumerate(x):
-            legendre_pp[i] = sc.misc.derivative(legendre_p,x_i)
-    else:
-        legendre_pp = sc.misc.derivative(legendre_p,x)
-    return legendre_pp
-
-def legendre_double_prime_recursive(x,n):
-    legendre_pp = 2 * x * legendre_prime(x,n) - n*(n+1)*legendre(n)(x)
-    return legendre_pp * (1-x**2)
-
-
-def grid_gauss_lobatto(a, b, n):
-    # roots of chebichev polynomials
-    x_0 = np.cos(np.arange(1,n)/n*np.pi)
-    # print('second derivatives recursive : ', legendre_double_prime_recursive(x_0,n))
-    # print('second derivatives numerical : ', legendre_double_prime_num(x_0,n))
-    nodal_pts = np.zeros(len(x_0)+2)
-    for i, ch_pt in enumerate(x_0):
-        leg_p = partial(legendre_prime, n=n)
-        leg_pp = partial(legendre_double_prime_recursive, n=n)
-        nodal_pts[i+1] = newton_method(leg_p, leg_pp, ch_pt, 40)[0]
-    nodal_pts[0] = -1
-    nodal_pts[-1] = 1
-    return (-nodal_pts+1)*(b-a)/2 + a
-
-
-def plot_grid(grids,show=True):
-    if np.ndim(grids)>1:
-        for i, grid in enumerate(grids):
-            plt.plot(grid, np.ones(np.size(grid))*i, '-o')
-    else:
-        plt.plot(grids, np.zeros(len(grids)), '-o')
-    plt.ylim(-1, 2)
-
-    if show:
-        plt.show()
-
-# Basis
-
-
-def basis_monomial(x, grid):
-    phi = np.zeros((len(grid), len(x)))
-    for i in range(len(grid)):
-        phi[i,:] = x**i
-    return phi
-
-
-def basis_newton(x, grid):
-    phi = np.ones((len(grid), len(x)))
-    for i in range(len(grid)):
-        for j in range(i):
-            phi[i,:] *= (x-grid[j])
-    return phi
-
-
-def basis_lagrange(x,grid):
-    phi = np.ones((len(grid), len(x)))
-    for j in range(len(grid)):
-        for i in range(len(grid)):
-            if i != j:
-                phi[j] *= (x-grid[i])/(grid[j]-grid[i])
-    return phi
-
-
-def plot_basis(x, phi, grid_2):
-    for base in phi:
-        plt.plot(x,base)
-    plot_grid(grid_2,show=False)
-    plt.show()
-
-# Solving interpolation condition A x = f
+    def plot(self):
+        for base in self.basis:
+            plt.plot(self.x,base)
+        self.grid.plot()
 
 
 def find_interpolation_coeff(type, grid, f):
-    A = np.zeros((np.size(grid),np.size(grid)))
-    b = f(grid)
+    my_basis = PolyBasis(grid.nodal_pts,grid)
+    # Vandermonde matrix
+    A = np.zeros((np.size(grid.nodal_pts),np.size(grid.nodal_pts)))
+    b = f(grid.nodal_pts)
     if type == 'lagrange':
-        phi = basis_lagrange(grid,grid)
+        my_basis.lagrange()
     elif type == 'newton':
-        phi = basis_newton(grid,grid)
+        my_basis.newton()
     elif type == 'monomial':
-        phi = basis_monomial(grid,grid)
-    for i,base in enumerate(phi):
+        my_basis.monomial()
+    for i,base in enumerate(my_basis.basis):
         A[:,i] = base
     coeff = np.linalg.solve(A,b)
     return coeff, A
 
-# reconstruction
 
-def reconstruct(coeff, phi):
-    return coeff.dot(phi)
+def reconstruct(coeff, basis):
+    '''
+    Reconstruct polynomial from basis
+    '''
+    return coeff.dot(basis.basis)
 
 
 def plot_interpolation(f, interpolated_f, x):
@@ -183,22 +79,36 @@ def plot_interpolation(f, interpolated_f, x):
     error_max = np.max(error)
     return error_max
 
+
 def plot_funcs(funcs):
-    a = -10
-    b = 10
-    grid = grid_chebychev(a,b,50)
-    x = np.linspace(a,b,101)
+    a, b = -5, 5
+    x_1 = np.linspace(a, b, 101)
+
+    grid_1 = Grid(a, b, 20)
+    grid_1.gauss_lobatto()
+
+    basis_1 = PolyBasis(x_1, grid_1)
+    basis_1.lagrange()
+
     for func in funcs:
-        coeff, A = find_interpolation_coeff('lagrange', grid, func)
-        phi = basis_lagrange(x, grid)
-        interpolated_f = reconstruct(coeff, phi)
-        error = plot_interpolation(func, interpolated_f, x)
+        coeff, A = find_interpolation_coeff('lagrange', grid_1, func)
+        interpolated_f = reconstruct(coeff, basis_1)
+        error = plot_interpolation(func, interpolated_f, x_1)
         print('Error max: ', error)
 
 if __name__ == '__main__':
-    grid_0 = Grid(-1,1,5)
-    uni_grid_0 = grid_0.uniform()
-    plot_grid(uni_grid_0)
+    a,b = -1,1
+    x = np.linspace(a,b, 1001)
+
+    grid = Grid(a,b,3)
+    grid.gauss_lobatto()
+
+    basis = PolyBasis(x,grid)
+    basis.lagrange()
+    # basis.plot()
+
+    # grid_0.plot()
+
     # grid = grid_chebychev(-1,1,3)
     # grid_2 = grid_uniform(-2,2,3)
     # print('Chebishev grid ', grid)
@@ -235,5 +145,5 @@ if __name__ == '__main__':
 
     # # error tends to decrease with the Chebischev nodes
     # print('Error max: ', error)
-    # funcs = [f,f_poly5,f_abs,f_abs3,f_gauss,f_runge,f_step]
-    # plot_funcs(funcs)
+    funcs = [f,f_poly5,f_abs,f_abs3,f_gauss,f_runge,f_step]
+    plot_funcs(funcs)
---
+++ grid.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+from legendre_functions import *
+from root_finding import newton_method
+from functools import partial
+
+
+class Grid:
+
+    def __init__(self,start,end,num):
+        self.a = start
+        self.b = end
+        self.n = num
+        self.nodal_pts = np.empty(num+1)
+
+    def linear_scaling(self):
+        """
+        linear map : [-1,1] --> [a,b]
+        """
+        self.nodal_pts = (-self.nodal_pts + 1) * (self.b - self.a) / 2 + self.a
+
+    def uniform(self):
+        self.nodal_pts = np.linspace(self.a, self.b, self.n+1)
+        return self.nodal_pts
+
+    def chebychev(self):
+        """
+        Calculates roots of the n+1 Chebychev polynomial of the first king
+        returns: nodal pts
+        """
+        i = np.arange(0, self.n + 1)
+        # nodal point in [-1,1]
+        self.nodal_pts = np.cos((2 * i + 1) / (2 * (self.n + 1)) * np.pi)
+        self.linear_scaling()
+        return self.nodal_pts
+
+    def gauss_lobatto(self):
+        """
+        Calculates the roots of (1+x**2)*L'_n(x)
+        :return: nodal pts
+        """
+        # Chebychev nodes as IV for newton method
+        x_0 = np.cos(np.arange(1,self.n)/self.n*np.pi)
+        # Last and first pts are fixed for every n
+        # This line is introduced to avoid modifying previously calculated nodal_pts
+        self.nodal_pts = np.empty(self.n+1)
+        self.nodal_pts[-1] = -1
+        self.nodal_pts[0] = 1
+        for i, ch_pt in enumerate(x_0):
+            leg_p = partial(legendre_prime, n=self.n)
+            leg_pp = partial(legendre_double_prime_recursive, n=self.n)
+            self.nodal_pts[i+1] = newton_method(leg_p, leg_pp, ch_pt, 40)[0]
+        self.linear_scaling()
+        return self.nodal_pts
+
+    def plot(self,show=True):
+        plt.plot(self.nodal_pts, np.zeros(self.n+1), '-o')
+        plt.ylim(-1, 2)
+        if show:
+            plt.show()
+
+    def show(self):
+        print('Nodal point of the grid: \n', self.nodal_pts)
+
+
+def plot_grids(grids,show=True):
+    if np.ndim(grids)>1:
+        for i, grid in enumerate(grids):
+            plt.plot(grid, np.ones(np.size(grid))*i, '-o')
+    else:
+        plt.plot(grids, np.zeros(len(grids)), '-o')
+    plt.ylim(-1, np.ndim(grids)+1)
+
+    if show:
+        plt.show()
+
+
+# if __name__ == '__main__':
+#     grid_0 = Grid(-1,1,7)
+#
+#     uni_grid_0 = grid_0.uniform()
+#     lob_grid_0 = grid_0.gauss_lobatto()
+#     ch_grid_0 = grid_0.chebychev()
+#
+#     show_grid(ch_grid_0)
+#     show_grid(uni_grid_0)
+#     show_grid(lob_grid_0)
+#
+#     plot_grids([uni_grid_0,ch_grid_0,lob_grid_0])
