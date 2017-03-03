96e1f19d87f18e9938a3a9200e3b418cd5dba0d4
Lorenzo <lorenzoterenzi96@gmail.com>
2017-03-03 19:29:47 +0100

    Changed import statements

--- grid.py
+++ grid.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
-from legendre_functions import *
+import numpy as np
+from legendre_functions import legendre_prime, legendre_double_prime_recursive
 from root_finding import newton_method
 from functools import partial
 
 
 class Grid:
 
-    def __init__(self,start,end,num):
+    def __init__(self, start, end, num):
         self.a = start
         self.b = end
         self.n = num
-        self.nodal_pts = np.empty(num+1)
+        self.nodal_pts = np.empty(num + 1)
 
     def linear_scaling(self):
         """
@@ -19,7 +20,7 @@ class Grid:
         self.nodal_pts = (-self.nodal_pts + 1) * (self.b - self.a) / 2 + self.a
 
     def uniform(self):
-        self.nodal_pts = np.linspace(self.a, self.b, self.n+1)
+        self.nodal_pts = np.linspace(self.a, self.b, self.n + 1)
         return self.nodal_pts
 
     def chebychev(self):
@@ -39,21 +40,23 @@ class Grid:
         :return: nodal pts
         """
         # Chebychev nodes as IV for newton method
-        x_0 = np.cos(np.arange(1,self.n)/self.n*np.pi)
+        x_0 = np.cos(np.arange(1, self.n) / self.n * np.pi)
         # Last and first pts are fixed for every n
-        # This line is introduced to avoid modifying previously calculated nodal_pts
-        self.nodal_pts = np.empty(self.n+1)
+        # This line is introduced to avoid modifying previously calculate
+        # nodal
+        self.nodal_pts = np.empty(self.n + 1)
         self.nodal_pts[-1] = -1
         self.nodal_pts[0] = 1
         for i, ch_pt in enumerate(x_0):
+
             leg_p = partial(legendre_prime, n=self.n)
             leg_pp = partial(legendre_double_prime_recursive, n=self.n)
-            self.nodal_pts[i+1] = newton_method(leg_p, leg_pp, ch_pt, 40)[0]
+            self.nodal_pts[i + 1] = newton_method(leg_p, leg_pp, ch_pt, 40)[0]
         self.linear_scaling()
         return self.nodal_pts
 
     def plot(self, show=True):
-        plt.plot(self.nodal_pts, np.zeros(self.n+1), '-o')
+        plt.plot(self.nodal_pts, np.zeros(self.n + 1), '-o')
         if show:
             plt.show()
 
@@ -62,30 +65,31 @@ class Grid:
 
 
 def plot_grids(grids, *args, show=True, save=False):
-    if np.ndim(grids)>1:
+    if np.ndim(grids) > 1:
         for i, grid in enumerate(grids):
             if args:
-                plt.plot(grid, np.ones(np.size(grid))*i, '-o', label=args[i])
+                plt.plot(grid, np.ones(np.size(grid)) * i, '-o', label=args[i])
             else:
                 plt.plot(grid, np.ones(np.size(grid)) * i, '-o')
     else:
         plt.plot(grids, np.zeros(len(grids)), '-o')
-    plt.title(args[-1] + 'for N = {0}' .format(np.size(grids[0])-1))
+    plt.title(args[-1] + "for N = {0}" .format(np.size(grids[0]) - 1))
     plt.xlabel(r'$\xi$')
-    plt.ylim(-1, np.ndim(grids)+1)
+    plt.ylim(-1, np.ndim(grids) + 1)
     plt.legend()
     if save:
-        plt.savefig('../Images_numerical/Grid_N_' + str(np.size(grids[0])-1) + '.png', bbox_inches='tight')
+        plt.savefig('../Images_numerical/Grid_N_' +
+                    str(np.size(grids[0]) - 1) + '.png', bbox_inches='tight')
     if show:
         plt.show()
 
 
 if __name__ == '__main__':
-    grid_0 = Grid(-1,1,4)
+    grid_0 = Grid(-1, 1, 4)
 
     uni_grid_0 = grid_0.uniform()
     lob_grid_0 = grid_0.gauss_lobatto()
     ch_grid_0 = grid_0.chebychev()
 
-    plot_grids([uni_grid_0, ch_grid_0, lob_grid_0], 'Uniform', 'Chebychev', 'Gauss-Lobatto',
-               'Grid comparison', save=True)
+    plot_grids([uni_grid_0, ch_grid_0, lob_grid_0], 'Uniform',
+               'Chebychev', 'Gauss-Lobatto', 'Grid comparison', save=False)
